10e9a16a9092f3cb9d7c43cd9d6057fe4208f3b1
Will Rolls <32841471+RollsW@users.noreply.github.com>
2023-09-18 22:01:14 +0100

    Added pangram including all runes (not just English letters)

--- runic.py
+++ runic.py
@@ -74,16 +74,25 @@ def runify(text):
 
 
 if __name__ == "__main__":
-    # With apologies to the late Prof Tolkien.
-    text = """Where now the horse and the rider? Where is the horn that was blowing? 
-Where is the helm and the hauberk, and the bright hair flowing? 
-Where is the hand on the harpstring, and the red fire glowing? 
-Where is the spring and the harvest and the tall corn growing? 
-They have passed like rain on the mountain, like a wind in the meadow; 
-The days have gone down in the West behind the hills into shadow. 
-Who shall gather the smoke of the dead wood burning, 
+    #This is a pangram which includes all of the letters in English as well as
+    #runes which represent a short string of letters ("ing" for example)
+    text = """sphinx of the black quartz ankh, tread my steep path via the 
+station braes to fairly judge each arising poetic vow through aeons"""
+
+    # With apologies to Prof Tolkien.
+    text = """Where now the horse and the rider? Where is the horn that was blowing?
+Where is the helm and the hauberk, and the bright hair flowing?
+Where is the hand on the harpstring, and the red fire glowing?
+Where is the spring and the harvest and the tall corn growing?
+They have passed like rain on the mountain, like a wind in the meadow;
+The days have gone down in the West behind the hills into shadow.
+Who shall gather the smoke of the dead wood burning,
 Or behold the flowing years from the Sea returning?"""
-    #    for k,v in runic_dict.items():
-    #        print(f"{k} - {v}"
-    print(text)
+
+    print(text, end="\n\n")
     print(runify(text))
+
+#     x =  runic_dict.items()
+
+#     for k,v in x:
+#         print(f"{k}\t- {v}")
